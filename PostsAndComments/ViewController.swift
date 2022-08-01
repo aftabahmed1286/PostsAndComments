d2bd52f86ee671802607e63b50a6b76d9876e7f5
Aftab Ahmed <aahmed03@in.ibm.com>
2022-08-02 00:26:52 +0530

    initial commit

---
+++ PostsAndComments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PostsAndComments
+//
+//  Created by AftabAhmed on 29/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
